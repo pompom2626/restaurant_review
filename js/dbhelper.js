f652fce8a0f5983de9ed525765795a5ef0ca54e7
pompom2626 <pompom2626@gmail.com>
2020-01-10 02:23:14 -0600

    4th-4 : to fix img url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,7 +153,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`restaurant_review/img/${restaurant.photograph}`);
   }
 
   /**
