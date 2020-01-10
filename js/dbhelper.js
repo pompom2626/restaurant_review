ef32d795f9e80375986c8ee7dd98c6905cd9d4e0
pompom2626 <pompom2626@gmail.com>
2020-01-10 00:53:45 -0600

    4th : to change return address fromm offline to github and ready to responsive app

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -11,7 +11,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 5500 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return 'https://github.com/pompom2626/restaurant_review/blob/master/data/restaurants.json';
+    //return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
